93d13ed2687ff4a345d43c3d102530f48e7dd653
RayhanalKavey <rayhanalkavey@gmail.com>
2023-02-13 14:03:23 +0600

    redux toolkit initila store creation and authSlice creation.

--- src/features/auth/authSlice.js
+++ src/features/auth/authSlice.js
@@ -1,1 +1,13 @@
 import { createSlice } from "@reduxjs/toolkit";
+const initialState = {
+  email: "",
+  role: "",
+  isLoading: true,
+  isError: false,
+  error: "",
+};
+const authSlice = createSlice({
+  name: "auth",
+  initialState,
+});
+export default authSlice.reducer;
